2a028934debd0012397eaad17a5f79b20ab24fbc
BytefarmerFarmsBytes <106711433+BytefarmerFarmsBytes@users.noreply.github.com>
2024-03-25 21:02:09 -0400

    Delete files directory

--- files/ui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-#GUI for TRIM4SSD-portable...
-#
-
-#
-yad --center  --title="Action Light Changer" --form --width=400 --text="Select your color" \
---button="Red":light\ -s\ red --button="Green":light\ -s\ red --button="Blue":light\ -s\ red --button="Cyan":light\ -s\ red --button="Magenta":light\ -s\ red --button="Yellow":light\ -s\ red --button="White":light\ -s\ red --button="Black":light\ -s\ red
